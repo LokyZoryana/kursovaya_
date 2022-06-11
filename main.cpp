25d6ea0d4ae067da9be941c9ab87705011950262
LokyZoryana <99095209+LokyZoryana@users.noreply.github.com>
2022-06-11 03:27:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <exception>
-#include "Person.h"
 #include "Doctor.h"
 #include "Soldier.h"
 using namespace std;
@@ -9,16 +8,9 @@ using namespace std;
 
 int main()
 {
-    //string firstName;
-    //string  lastName;
-    //int age; int height; int weight;
-    //string gender; string mail; string city; string country;enum Rank rank; int AmountOfFights;
-    //int LifeTime; string Weapon; TypeOfService typeOfService;  string Speciality; int AmountOfPatients;
-    //string Category; string Degree;
-
     try{
         Doctor doctor1("Irina", "Nikolaevna", 30, 175, 60, "female","chdghch@gmail.com",
-                       "Odesa", "Ukraine", "",300 ,"Gynecologist", "First");
+                       "Odesa", "Ukraine", "Gynecology",300 ,"Gynecologist", "First");
         doctor1.Consultation();
         cout<< doctor1.Info()<<endl;
         cout<< "Consultation price - "<< doctor1.Consultation()<<endl;
@@ -30,7 +22,7 @@ int main()
 
 
         Doctor doctor2("Michael", "Ivanovich", 29, 195, 70, "male","gjfdhgj@gmail.com",
-                       "Lviv", "Ukraine", "",140 ,"Ophthalmologist", "Second");
+                       "Lviv", "Ukraine", "Ophthalmology",140 ,"Ophthalmologist", "Second");
         doctor2.Consultation();
         cout<< doctor2.Info()<<endl;
         cout<< "Consultation price - "<< doctor2.Consultation()<<endl;
@@ -42,7 +34,7 @@ int main()
 
 
         Doctor doctor3("Ivan", "Victorovich", 60, 189, 70, "male","jidsjf@gmail.com",
-                       "Kherson", "Ukraine", "",678 ,"Surgeon", "Highest");
+                       "Kherson", "Ukraine", "Surgery",678 ,"Surgeon", "Highest");
         doctor3.Consultation();
         cout<< doctor3.Info()<<endl;
         cout<< "Consultation price - "<< doctor3.Consultation()<<endl;
@@ -62,7 +54,7 @@ int main()
 
         cout<< "Is Adult? - "<<(soldier1.isAdult()?"18+":"less then 18");
         cout<<endl;
-        cout<< (soldier1.militaryAge()?"Yes":"No");
+        cout<< "Is military age? - "<<(soldier1.militaryAge()?"Yes":"No");
         cout<< endl;
         cout<< "Is the height big?  - "<<(soldier1.getisbigheight()?"Yes":"No");
         cout<< endl;
@@ -81,7 +73,7 @@ int main()
 
         cout<< "Is Adult? - "<<(soldier2.isAdult()?"18+":"less then 18");
         cout<<endl;
-        cout<< (soldier2.militaryAge()?"Yes":"No");
+        cout<< "Is military age? - "<<(soldier2.militaryAge()?"Yes":"No");
         cout<< endl;
         cout<< "Is the height big?  - "<<(soldier2.getisbigheight()?"Yes":"No");
         cout<< endl;
@@ -91,7 +83,7 @@ int main()
 
 
         Soldier soldier3("Anton", "Petrovich", 20, 188, 70, "male","fbzszgga@gmail.com",
-                         "Odessa", "Ukraine", First, 6, 14,"Gun", Spare);
+                         "Odessa", "Ukraine", Second, 6, 14,"Gun", Spare);
 
         soldier3.NewFight();
         soldier3.GoSpare();
